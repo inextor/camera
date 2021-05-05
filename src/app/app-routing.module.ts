00b386a533c6236bb1a7cae0052b6f09576033ca
Nextor <nextor@gmail.com>
2021-05-05 12:19:05 -0700

    It works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import {CustomCameraComponent} from './pages/custom-camera/custom-camera.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: CustomCameraComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToastErrorComponent } from './components/toast-error/toast-error.component';
+import { CustomCameraComponent } from './pages/custom-camera/custom-camera.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToastErrorComponent,
+    CustomCameraComponent
   ],
   imports: [
     BrowserModule,
